7611337b43baeff0d10659fe073b8f2e350324d3
leoohfnds <43688073+leoohfnds@users.noreply.github.com>
2019-09-20 13:44:23 -0300

    Add files via upload

--- src/main/java/com/exemple/controlefinancerio/api/resource/CategoriaResource.java
+++ src/main/java/com/exemple/controlefinancerio/api/resource/CategoriaResource.java
@@ -6,13 +6,17 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +55,34 @@ public class CategoriaResource {
 
 		Optional<Categoria> status = categoriaRepository.findById(codigo);
 
-		return status != null ? ResponseEntity.ok(status) : ResponseEntity.notFound().build();
+		if (status.isPresent() == false) {
+
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(status);
+	}
+
+	@DeleteMapping("{/codigo}")
+	public void deletar(@PathVariable Long codigo) {
+		categoriaRepository.deleteById(codigo);
+	}
+
+	@PutMapping
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @Valid @RequestBody Categoria categoria) {
+
+		Optional<Categoria> categoriaSalva = categoriaRepository.findById(codigo);
+
+		if (categoriaSalva.isPresent() == false) {
+			throw new EmptyResultDataAccessException(1);
+		}
+
+		Categoria concategoriaSalva = categoriaSalva.get();
+
+		BeanUtils.copyProperties(categoria, concategoriaSalva, "codigo");
+		categoriaRepository.save(concategoriaSalva);
+
+		return ResponseEntity.ok(concategoriaSalva);
+
 	}
 
 }
--- src/main/java/com/exemple/controlefinancerio/api/resource/PessoaResource.java
+++ src/main/java/com/exemple/controlefinancerio/api/resource/PessoaResource.java
@@ -6,13 +6,17 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +31,7 @@ public class PessoaResource {
 
 	@Autowired
 	PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
@@ -43,7 +47,7 @@ public class PessoaResource {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 
@@ -51,9 +55,35 @@ public class PessoaResource {
 	public ResponseEntity<Optional<Pessoa>> buscarPorCodigo(@PathVariable Long codigo) {
 
 		Optional<Pessoa> status = pessoaRepository.findById(codigo);
+		
+		if (status.isPresent() == false) {
+			return ResponseEntity.notFound().build();
+		}
+			return ResponseEntity.ok(status);
+		
+	}
 
-		return status != null ? ResponseEntity.ok(status) : ResponseEntity.notFound().build();
+	@DeleteMapping("/{codigo}")
+	public void deletar(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
+	}
 
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
+		
+		if (pessoaSalva.isPresent() == false) {
+			
+			throw new EmptyResultDataAccessException(1);
+			
+		}
+		Pessoa conpessoaSalva = pessoaSalva.get(); 		// aqui eu converto pessoa salva para a entidade Pessoa. pq save aceita apenas Pessoa.
+		
+		BeanUtils.copyProperties(pessoa, conpessoaSalva, "codigo");
+		pessoaRepository.save(conpessoaSalva);
+		
+		return ResponseEntity.ok(conpessoaSalva);
+		
 	}
 
 }
