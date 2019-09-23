951b2b0bf4abc031b539ea68a4a20cad8b745e26
leoohfnds <43688073+leoohfnds@users.noreply.github.com>
2019-09-23 15:44:45 -0300

    Add files via upload

--- src/main/java/com/exemple/controlefinancerio/api/service/PessoaService.java
+++ src/main/java/com/exemple/controlefinancerio/api/service/PessoaService.java
@@ -12,23 +12,35 @@ import com.exemple.controlefinancerio.api.repository.PessoaRepository;
 
 @Service
 public class PessoaService {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
 
 	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
-		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
-		
-		if (pessoaSalva.isPresent() == false) {
-			
-			throw new EmptyResultDataAccessException(1);
-			
-		}
-		Pessoa conpessoaSalva = pessoaSalva.get(); 		// aqui eu converto pessoa salva para a entidade Pessoa. pq save aceita apenas Pessoa.
-		
+		Optional<Pessoa> pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		Pessoa conpessoaSalva = pessoaSalva.get(); // aqui eu converto pessoa salva para a entidade Pessoa. pq save
+													// aceita apenas Pessoa.
+
 		BeanUtils.copyProperties(pessoa, conpessoaSalva, "codigo");
 		return pessoaRepository.save(conpessoaSalva);
 	}
-	
+
+	public void AtualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Optional<Pessoa> pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		Pessoa pessoaCon = pessoaSalva.get();
+		pessoaCon.setAtivo(ativo);
+
+		pessoaRepository.save(pessoaCon);
+	}
+
+	private Optional<Pessoa> buscarPessoaPeloCodigo(Long codigo) {
+		Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
+
+		if (pessoaSalva.isPresent() == false) {
+
+			throw new EmptyResultDataAccessException(1);
+
+		}
+		return pessoaSalva;
+	}
 }
